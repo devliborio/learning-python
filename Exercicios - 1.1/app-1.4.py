8fbb33758842773b5c86b752404c5f3d6eb296cb
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-01-16 18:37:40 -0300

    Enviando Exercicio

---
+++ Exercicios - 1.1/app-1.4.py
@@ -0,0 +1,13 @@
+h = float(input('Quanto você ganha por hora? ')) # 9 reais
+hM = float(input('Quantas horas você trabalhou esse mês? ')) # 120
+
+if h and hM:
+    s = h * hM
+    print('Seu salario é: R${}'.format(s))
+
+
+
+
+# 9 reais por hora - 6 horas por dia - 54 reais por dia.
+# 270 reais por semana.
+# 270 reais x 4 semanas = 1,080 reais
