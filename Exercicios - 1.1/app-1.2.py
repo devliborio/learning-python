af030a97de51bd65c74555c29fbe5aac4af2767c
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-01-16 17:52:44 -0300

    Enviando tarefa

---
+++ Exercicios - 1.1/app-1.2.py
@@ -0,0 +1,16 @@
+metroCentimetro = int(input('Digite quantos metros você quer converter para centimetro: '))
+
+if metroCentimetro:
+    centimetro = metroCentimetro * 100
+    print('Fica: {} centimetros'.format(centimetro))
+
+
+
+
+
+
+
+# metros = int(20)
+
+# centimetros = metros * 100
+# print('20 metros é igual a: ',centimetros)
