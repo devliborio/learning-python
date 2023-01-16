f5ca37a5fa4d8a029fc9af76a3b7e151bc32270a
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-01-16 16:46:21 -0300

    Enviando Exercicios

---
+++ Exercicios/app-8-(Em revisão).py
@@ -0,0 +1,33 @@
+combustivel = str(input('Digite o tipo de combusitvel: ')).strip().capitalize()
+
+litro = float(input('Digite a quantidade de litros: '))
+
+if combustivel == 'A':
+    alcool = 3.5
+    custo = litro * alcool
+
+
+if litro <= 20:
+    desconto1 = (custo * 3)/100
+    print("O desconto de combustivel foi de: {} e o preço agora é {}".format(desconto1,custo-desconto1))
+
+
+elif litro > 20:
+    desconto2 = (custo * 5)/100
+    print("O desconto de combustivel foi de: {} e o preço agora é {}".format(desconto2, custo-desconto2))
+
+
+
+# elif combustivel == 'G':
+#     gasolina = 3.5
+#     custo = litro * gasolina
+
+
+# if litro <= 20:
+#     desconto3 = (custo * 4)/100
+#     print("O desconto de combustivel foi de: {} e o preço agora é {}".format(desconto3, custo-desconto3))
+
+
+# elif litro > 20:
+#     desconto4 = (custo * 6)/100
+#     print("O desconto de combustivel foi de: {} e o preço agora é {}".format(desconto4, custo-desconto4))
