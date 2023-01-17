94ab0f7350f5aab289d1f41b9f96773f1e5d0b5e
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-01-16 22:28:48 -0300

    Enviando Exercicio

---
+++ Exercicios - 1.1/app-1.7.py
@@ -0,0 +1,11 @@
+numero1 = int(input('Digite um número inteiro: '))
+numero2 = int(input('Digite outro número inteiro: '))
+
+if numero1 > numero2:
+  print(numero1)  
+else:
+  print(numero2)
+
+
+# print('Número {} é maior que {}'.format(numero1,numero2))
+# print('Número {} é maior que {}'.format(numero2,numero1))
