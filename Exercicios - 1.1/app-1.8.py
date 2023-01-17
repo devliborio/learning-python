9a1401f55e962e3403a30101c5f00114df43ac48
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-01-16 22:31:47 -0300

    Enviando Exercicio

---
+++ Exercicios - 1.1/app-1.8.py
@@ -0,0 +1,6 @@
+numeroDigitado = int(input('Digite um número: '))
+
+if numeroDigitado >= 0:
+    print('{} é positivo!'.format(numeroDigitado))
+else:
+    print('{} é negativo'.format(numeroDigitado))
