39ecd37e93ba32a6053a47aca0e7a5623d52c3e8
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-05 23:08:56 -0300

    update

--- Treinamento/app-1.16.py
+++ Treinamento/app-1.16.py
@@ -1,10 +1,9 @@
 helloWorld = input("Digite o seu primeiro 'Olá mundo!': ")
 print(helloWorld.upper())
 
-
 # ou
 
 helloWorld2 = int(input("Digite um número agora: "))
 
-print(f"Python é bem legalzinho até, tá aqui o seu número {helloWorld2}")
+print(f"Python é bem interessante, tá aqui o seu número {helloWorld2}")
 print(type(helloWorld2))
